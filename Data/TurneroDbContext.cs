57db32e17a2ca9478c8a0a236db5581015160109
StJimmy130 <nicolasmaidana811@gmail.com>
2025-10-02 22:18:04 -0300

    Configuraciones basicas

---
+++ Data/TurneroDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Turnero.Data;
+
+public class TurneroDbContext : IdentityDbContext
+{
+    public TurneroDbContext(DbContextOptions<TurneroDbContext> options)
+        : base(options)
+    {
+    }
+}
